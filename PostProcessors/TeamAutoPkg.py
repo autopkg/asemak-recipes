6718c7731bea6851797ffed74425368dbe2b6045
Andy Semak <andy.semak@ntu.ac.uk>
2019-02-19 13:21:26 +0000

    Update TeamAutoPkg.py

--- PostProcessors/TeamAutoPkg.py
+++ PostProcessors/TeamAutoPkg.py
@@ -77,8 +77,7 @@ class TeamAutoPkg(Processor):
             if name:
                 teams_text = "*New item added to repo:*\nTitle: *%s*\nVersion: *%s*\nCatalog: *%s\n*Pkg Path: *%s*\nPkginfo Path: *%s*" % (
                     name, version, catalog, pkg_path, pkginfo_path)
-                teams_data = {
-                    'text': teams_text }
+                teams_data = {'text': teams_text}
 
                 response = requests.post(webhook_url, json=teams_data)
                 if response.status_code != 200:
